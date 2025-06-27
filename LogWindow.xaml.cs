b0acc42184c95aaa5b76d102f933d8ec2bf717c0
Liam Brown <131519232+LiamB1017@users.noreply.github.com>
2025-06-27 13:02:01 +0200

    Added logic behind log window

--- LogWindow.xaml.cs
+++ LogWindow.xaml.cs
@@ -14,14 +14,28 @@ using System.Windows.Shapes;
 
 namespace CyberSecurityChatbotGUI
 {
-    /// <summary>
-    /// Interaction logic for LogWindow.xaml
-    /// </summary>
+    /// <summary>  
+    /// Interaction logic for LogWindow.xaml  
+    /// </summary>  
     public partial class LogWindow : Window
     {
-        public LogWindow(List<String> ActivityLog)
+        public LogWindow(List<string> activityLog)
         {
             InitializeComponent();
+            Log = activityLog;
+            ShowRecent(Log);
+        }
+
+        public List<string> Log { get; private set; }
+
+        private void ShowRecent(List<string> log)
+        {
+            var recent = log.Count > 10 ? log.GetRange(log.Count - 10, 10) : log;
+            foreach (var item in recent)
+            {
+                LogList.Items.Add(item);
+            }
         }
     }
 }
+
